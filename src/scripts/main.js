4ade6f10c166f19b77408a9ad990d834ee7fabdb
David Senra <davidsenra33@gmail.com>
2023-05-25 18:57:48 -0300

    primeiro commit

---
+++ src/scripts/main.js
@@ -0,0 +1,267 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const hamburgerIcon = document.getElementById("hamburger");
+
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    const accordionButton = document.querySelector('[data-accordion]');
+    let slidesFoto = document.getElementById("fotos");
+    const slidesContainer = document.getElementById("slider-container");
+    const prevButton = document.getElementById("slide-arrow-prev");
+    const nextButton = document.getElementById("slide-arrow-next");
+    let slideAtual = 1;
+
+    const botoesVideogame = document.querySelectorAll('.videogames');
+    const textosVideogame = document.querySelectorAll('.textos-videogames');
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    accordionButton.addEventListener('click', abreOuFechaAccordion);
+
+    hamburgerIcon.addEventListener('mouseover', hoverHamburger);
+    hamburgerIcon.addEventListener('mouseleave', defaultHamburger);
+    hamburgerIcon.addEventListener('click', menuHamburger);
+
+    prevButton.addEventListener("click", () => {
+        var window_width = window.innerWidth;
+        let slideWidth = 0;
+        console.log(window.innerWidth);
+        if (window_width > 1500) {
+            slideWidth = slidesContainer.offsetWidth/3.1;
+        }
+        else if (window_width > 1000) {
+            slideWidth = slidesContainer.offsetWidth/3;
+        }
+        else if (window_width > 900) {
+            slideWidth = slidesContainer.offsetWidth/2.2;
+        }
+        else if (window_width > 700) {
+            slideWidth = slidesContainer.offsetWidth;
+        }
+        else {
+            slideWidth = slidesContainer.offsetWidth;
+        }
+
+        if (slideAtual > 3) {
+        }
+        else if (slideAtual > 2) {
+            slidesContainer.scrollLeft += slideWidth;
+            prevButton.classList.remove('elenco__container__list__item--button__button--activated');
+            prevButton.classList.add('elenco__container__list__item--button__button--deactivated');
+            nextButton.classList.add('elenco__container__list__item--button__button--activated');
+            nextButton.classList.remove('elenco__container__list__item--button__button--deactivated');
+            slideAtual++;
+        }
+        else {
+            slidesContainer.scrollLeft += slideWidth;
+            nextButton.classList.add('elenco__container__list__item--button__button--activated');
+            nextButton.classList.remove('elenco__container__list__item--button__button--deactivated');
+            slideAtual++;
+        }
+    });
+
+    nextButton.addEventListener("click", () => {
+        var window_width = window.innerWidth;
+        let slideWidth = 0;
+        console.log(window.innerWidth);
+        if (window_width > 1500) {
+            slideWidth = slidesContainer.offsetWidth/3.1;
+        }
+        else if (window_width > 1000) {
+            slideWidth = slidesContainer.offsetWidth/3;
+        }
+        else if (window_width > 900) {
+            slideWidth = slidesContainer.offsetWidth/2.2;
+        }
+        else if (window_width > 700) {
+            slideWidth = slidesContainer.offsetWidth;
+        }
+        else {
+            slideWidth = slidesContainer.offsetWidth;
+        }
+
+        if (slideAtual < 2) {
+        }
+        else if (slideAtual < 3) {
+            slidesContainer.scrollLeft -= slideWidth;
+            nextButton.classList.remove('elenco__container__list__item--button__button--activated');
+            nextButton.classList.add('elenco__container__list__item--button__button--deactivated');
+            prevButton.classList.add('elenco__container__list__item--button__button--activated');
+            prevButton.classList.remove('elenco__container__list__item--button__button--deactivated');
+            slideAtual--;
+        }
+        else {
+            slidesContainer.scrollLeft -= slideWidth;
+            prevButton.classList.add('elenco__container__list__item--button__button--activated');
+            prevButton.classList.remove('elenco__container__list__item--button__button--deactivated');
+            slideAtual--;
+        }
+    });
+
+    window.addEventListener('scroll', function() {
+        const scrollJanela = window.scrollY;
+        if (scrollJanela <= alturaHero) {
+            ocultaLinhaHeader();
+        }
+        else {
+            exibeLinhaHeader();
+        }
+    })
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function(botao) {
+            const abaAlvo = botao.target.dataset.tabButton;
+            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
+
+            escondeAbas();
+            aba.classList.add('series__container__list--is-active');
+
+            removeBotaoAtivo();
+            buttons[i].classList.add('title--series--selected');
+            buttons[i].classList.remove('title--series--not-selected');
+        })
+    }
+
+    for (let i = 0; i < botoesVideogame.length; i++) {
+        botoesVideogame[i].addEventListener('mouseover', function(botao) {
+            const botaoAlvo = botao.target;
+            imagemAlvo = botaoAlvo.src;
+            index = imagemAlvo.lastIndexOf("/");
+            filename = imagemAlvo;
+            if(index !== -1) {     
+                filename = imagemAlvo.substring(index+1,(imagemAlvo.length-4));
+            }
+            imagemAlvo = `./../../dist/images/jogos/${filename}-orange.png`;
+            botaoAlvo.src = imagemAlvo;
+        })
+        botoesVideogame[i].addEventListener('mouseleave', function(botao) {
+            const botaoAlvo = botao.target;
+            imagemAlvo = botaoAlvo.src;
+            index = imagemAlvo.lastIndexOf("/");
+            filename = imagemAlvo;
+            if(index !== -1) {     
+                filename = imagemAlvo.substring(index+1,(imagemAlvo.length-11));
+            }
+            imagemAlvo = `./../../dist/images/jogos/${filename}.png`;
+            botaoAlvo.src = imagemAlvo;
+        })
+    }
+
+    for (let i = 0; i < textosVideogame.length; i++) {
+        textosVideogame[i].addEventListener('mouseover', function(botao) {
+            const botaoAlvo = botao.target;
+            pai = botaoAlvo.parentElement;
+            let objeto = "";
+            if (pai.hasChildNodes()) {
+                let children = pai.childNodes;
+                objeto = children[1];
+            }
+            imagemAlvo = objeto.src;
+            index = imagemAlvo.lastIndexOf("/");
+            filename = imagemAlvo;
+            if(index !== -1) {     
+                filename = imagemAlvo.substring(index+1,(imagemAlvo.length-4));
+            }
+            imagemAlvo = `./../../dist/images/jogos/${filename}-orange.png`;
+            objeto.src = imagemAlvo;
+        })
+        textosVideogame[i].addEventListener('mouseleave', function(botao) {
+            const botaoAlvo = botao.target;
+            pai = botaoAlvo.parentElement;
+            let objeto = "";
+            if (pai.hasChildNodes()) {
+                let children = pai.childNodes;
+                objeto = children[1];
+            }
+            imagemAlvo = objeto.src;
+            index = imagemAlvo.lastIndexOf("/");
+            filename = imagemAlvo;
+            if(index !== -1) {     
+                filename = imagemAlvo.substring(index+1,(imagemAlvo.length-11));
+            }
+            imagemAlvo = `./../../dist/images/jogos/${filename}.png`;
+            objeto.src = imagemAlvo;
+        })
+    }
+
+    
+
+})
+
+function hoverHamburger(elemento) {
+    const hamburger = elemento.target;
+    imagemAlvo = hamburger.src;
+    index = imagemAlvo.lastIndexOf("/");
+    filename = imagemAlvo;
+    if(index !== -1) {     
+        filename = imagemAlvo.substring(index+1,(imagemAlvo.length-4));
+    }
+    imagemAlvo = `./../../dist/images/${filename}-hover.png`;
+    elemento.target.src = imagemAlvo;
+}
+
+function defaultHamburger(elemento) {
+    const hamburger = elemento.target;
+    imagemAlvo = hamburger.src;
+    index = imagemAlvo.lastIndexOf("/");
+    filename = imagemAlvo;
+    if(index !== -1) {     
+        filename = imagemAlvo.substring(index+1,(imagemAlvo.length-10));
+    }
+    imagemAlvo = `./../../dist/images/${filename}.png`;
+    elemento.target.src = imagemAlvo;
+}
+
+function menuHamburger() {
+    const menuSuspenso = document.getElementById("menu-suspenso");
+    classe = "header__menu-suspenso--is-open"
+    menuSuspenso.classList.toggle(classe);
+}
+
+function ocultaLinhaHeader(elemento) {
+    const header = document.querySelector('.header__menu-principal')
+    header.classList.add('header__menu-principal--is-hidden')
+    header.classList.remove('header__menu-principal--is-shown')
+}
+
+function exibeLinhaHeader(elemento) {
+    const header = document.querySelector('.header__menu-principal')
+    header.classList.remove('header__menu-principal--is-hidden')
+    header.classList.add('header__menu-principal--is-shown')
+}
+
+function removeBotaoAtivo() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('title--series--selected');
+        buttons[i].classList.add('title--series--not-selected');
+    }
+}
+
+function escondeAbas() {
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+
+    for (let i = 0; i < tabsContainer.length; i++) {
+            tabsContainer[i].classList.remove('series__container__list--is-active')
+    }
+}
+
+function testar(a) {
+    let frase = 'Mais Dispositivos';
+    return (a == frase ? 'Recolher' : frase);
+}
+
+function abreOuFechaAccordion(botao) {
+    const classe = 'accordion-group--is-open';
+    const classe2 = 'jogos__container__list__accordion--is-open';
+
+
+    const accordionSection = document.getElementById("accordion-section");
+    const accordionButton = document.querySelector('[data-accordion]');
+    const accordionText = document.querySelector('[text-accordion]');
+    const accordionTextHTML = accordionText.innerHTML;
+    
+    accordionSection.classList.toggle(classe);
+    accordionButton.classList.toggle(classe2);
+    accordionText.innerHTML = testar(accordionTextHTML);
+}
